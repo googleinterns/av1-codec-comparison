639d08553b7c48873c6b4fda66f84a47889c43c4
Nasir Hemed <nasirhemed@ymail.com>
2020-06-30 23:58:19 +0000

    Fix plural typos

--- rtc-video-quality/binary_vars.py
+++ rtc-video-quality/binary_vars.py
@@ -19,11 +19,11 @@ H264_ENC_BIN = 'openh264/h264enc'
 YAMI_ENC_BIN = 'yami/libyami/bin/yamiencode'
 VPX_SVC_ENC_BIN = 'libvpx/examples/vpx_temporal_svc_encoder'
 
-# Decoder Binary
+# Decoder Binaries
 AOM_DEC_BIN = 'aom/aom_build/aomdec'
 VPX_DEC_BIN = 'libvpx/vpxdec'
 H264_DEC_BIN = 'openh264/h264dec'
 
-# Metrics Binary
+# Metric Binaries
 TINY_SSIM_BIN = 'libvpx/tools/tiny_ssim'
 VMAF_BIN = 'vmaf/libvmaf/build/tools/vmafossexec'
